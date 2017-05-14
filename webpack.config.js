a980b987518875c0479a58bfdfffd3d4916c09bd
greggpatton <gregg.patton@gmail.com>
2017-05-14 14:44:20 -0500

    more config refinement

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,9 @@
 const webpack = require('webpack')
-const path = require('path')
 
 module.exports = {
   entry: [
     './src/webpack-entry.txt'
   ],
-  output: {
-    path: path.join(__dirname, '/dist'),
-    publicPath: '/',
-    filename: 'bundle.js'
-  },
   devServer: {
     contentBase: './dist',
     hot: true
